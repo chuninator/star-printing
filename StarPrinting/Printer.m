1ad11eed4664ee49e6831e2b2227cc84a720f583
Dave Carlton <davec@polymicro.net>
2016-10-26 10:37:15 -1000

    Issue_1 Corrected test for model number

--- StarPrinting/Printer.m
+++ StarPrinting/Printer.m
@@ -579,7 +579,8 @@ static char const * const ConnectJobTag = "ConnectJobTag";
     if ([p count] == 2) {
         
         NSString *modelNumber = p[0];
-        if ([modelNumber length] == 6 && [modelNumber rangeOfString:@"TSP1"].location != NSNotFound) {
+        NSRange result = [modelNumber rangeOfString:@"TSP1"];
+        if (result.location == NSNotFound) {
             compatible = NO;
         }
     }
